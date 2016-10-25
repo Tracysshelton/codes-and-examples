213ed5a411d64f46bcfcceeab4b7aeaf4163a397
Tracys <jimi1966@msn.com>
2016-10-25 10:25:32 -0700

    Create products.sql

---
+++ products.sql
@@ -0,0 +1,13 @@
+CREATE TABLE products(
+   sku    VARCHAR(9) NOT NULL PRIMARY KEY
+  ,price  VARCHAR(5) NOT NULL
+  ,qty    VARCHAR(3) NOT NULL
+  ,cost   VARCHAR(4) NOT NULL
+);
+INSERT INTO products(sku,price,qty,cost) VALUES ('sku','price','qty','cost');
+INSERT INTO products(sku,price,qty,cost) VALUES ('Keyboard','16','6','10');
+INSERT INTO products(sku,price,qty,cost) VALUES ('Monitor','120','14','80');
+INSERT INTO products(sku,price,qty,cost) VALUES ('Mouse','7','2','15');
+INSERT INTO products(sku,price,qty,cost) VALUES ('HardDrive','100','2','35');
+INSERT INTO products(sku,price,qty,cost) VALUES ('Laptop','125','5','75');
+INSERT INTO products(sku,price,qty,cost) VALUES ('Speakers','50','12','32');
